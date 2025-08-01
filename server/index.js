c8138aa59baf313dc7e30e1e3f9daae3d09a62e6
Abdelali Duiche <abdodui70@gmail.com>
2025-08-01 20:12:42 +0100

    Set up connection to mongodb via mongoose .

--- server/index.js
+++ server/index.js
@@ -1,16 +1,28 @@
 // IMPORTS FROM PACKAGES
 const express = require("express");
+const mongoose = require("mongoose");
 
 // IMPORTS FROM OTHER FILES
-const authRouter = require('./routes/auth');
+const authRouter = require("./routes/auth");
 
 // INIT
 const app = express();
 const PORT = 3000;
+const DB =
+  "mongodb+srv://Abdelali:xJjgjmtoyNiMS8Vf@cluster-1.byycnfm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-1";
 
 // MIDDLEWARE
 app.use(authRouter);
 
+// CONNECTIONS
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connected");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 app.listen(PORT, () => {
   console.log(`connected at port ${PORT}`);
