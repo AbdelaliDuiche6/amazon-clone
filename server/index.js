51b18f6bf7868ccaddb719460754cb31dac58339
Abdelali Duiche <abdodui70@gmail.com>
2025-08-01 14:20:24 +0100

    Add an authentication middleware to handle server/client communication.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
-// CREATING AN API
+// IMPORTS FROM PACKAGES
 const express = require("express");
+
+// IMPORTS FROM OTHER FILES
+const authRouter = require('./routes/auth');
+
+// INIT
 const app = express();
 const PORT = 3000;
+
+// MIDDLEWARE
+app.use(authRouter);
 
 
 app.listen(PORT, () => {
